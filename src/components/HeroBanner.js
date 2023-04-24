3d2a98722e789ec827bd68cc6e9208812b4b5cea
Aslam Khan shah <aksi34012@gmail.com>
2023-04-25 03:00:44 +0530

    .........

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,31 +1,36 @@
 import React from "react";
-import { Box, Stack, Typography , Button} from "@mui/material";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => {
   return (
     <Box
       sx={{
-        mt: { lg: "211px", xs: "70px" },
-        ml: { sm: "50px" },
+        mt: { lg: '212px', xs: '70px' },
+        ml: { sm: '50px' },
       }}
-      postion="relative"
-      p={20}
+      position={'relative'} p={'20px'}
     >
-      <Typography color={"#FF2625"} fontWeight={600} fontSize={26}>
+      <Typography color={'#FF2625'} fontWeight='600' fontSize='26px'>
         Fittness App
       </Typography>
 
-      <Typography fontWeight={700}
-      sx={{fontsize : {lg : "44px", xs : "40px"}}}>
+      <Typography
+        fontWeight={700}
+        sx={{ fontsize: { lg: "44px", xs: "40px" } }}
+      >
         Sweat , Smile <br /> and Repeat
       </Typography>
-      <Typography fontsize={'22px'} lineHeight={'35px'} mb={2}>check out the the most effective Exercises
+      <Typography fontsize={"22px"} lineHeight={"35px"} mb={2}>
+        check out the the most effective Exercises
       </Typography>
-      <Button variant="contained" color="error">
-        Explore Excerises 
+      <Button variant="contained" color="error" href="#Exercises">
+        Explore Excerises
       </Button>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
 };
 
-export default HeroBanner;
+export default HeroBanner
